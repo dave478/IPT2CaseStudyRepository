4bd64b1887412595e74fb3bc8386b1a63fabc666
Jonathan Dave Dela Cruz <79123361+dave478@users.noreply.github.com>
2021-03-19 12:03:16 +0800

    Update JavaCode.java
    Added 'circumference' and 'areacircle' methods

--- src/java/com/CaseStudy/JavaCode.java
+++ src/java/com/CaseStudy/JavaCode.java
@@ -22,3 +22,14 @@ public class JavaCode {
     return stepb;}
     
 }
+
+ @WebMethod
+    public double circumference(double r){
+    double answer=2*3.1415*r;
+    return answer;}
+    @WebMethod
+    public double areacircle(double r){
+    double answer=3.1415*(r*r);
+    return answer;}
+
+}
